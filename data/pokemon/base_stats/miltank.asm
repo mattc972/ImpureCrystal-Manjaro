f617e71c102f694076a9c464150d6be48118ddb0
mattc972 <crawford.mdc29@gmail.com>
2023-06-10 11:50:18 -0400

    Modified Miltank's Type

--- data/pokemon/base_stats/miltank.asm
+++ data/pokemon/base_stats/miltank.asm
@@ -3,7 +3,7 @@
 	db  95,  80, 105, 100,  40,  70
 	;   hp  atk  def  spd  sat  sdf
 
-	db NORMAL, NORMAL ; type
+	db NORMAL, GROUND ; type
 	db 45 ; catch rate
 	db 200 ; base exp
 	db MOOMOO_MILK, MOOMOO_MILK ; items

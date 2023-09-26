85c86d39f058be7e433acaf42168b4111f99c26e
mattc972 <crawford.mdc29@gmail.com>
2023-09-25 22:21:04 -0400

    Properly cased the Pokemon submenu (switching out/checking stats; both in and out of battle)

--- data/mon_menu.asm
+++ data/mon_menu.asm
@@ -12,13 +12,13 @@ DEF NUM_MONMENUVALUES EQU const_value - 1
 MonMenuOptionStrings:
 ; entries correspond to MONMENUVALUE_* constants
 	list_start MonMenuOptionStrings
-	li "STATS"
-	li "SWITCH"
-	li "ITEM"
-	li "CANCEL"
-	li "MOVE"
-	li "MAIL"
-	li "ERROR!"
+	li "Stats"
+	li "Switch"
+	li "Item"
+	li "Cancel"
+	li "Move"
+	li "Mail"
+	li "Error!"
 	assert_list_length NUM_MONMENUVALUES
 
 MonMenuOptions:
